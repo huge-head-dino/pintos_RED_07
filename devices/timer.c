214143fd1fba4f4f3b91c69503adb8c87f21d1c9
uujeen <rkddmlwls2@gmail.com>
2023-05-29 17:53:57 +0900

    :white_check_mark: Complete alarm clock
    complete alarm clock

--- devices/timer.c
+++ devices/timer.c
@@ -129,6 +129,7 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED) {
 	ticks++;
 	thread_tick (); // update the CPU usage for running process
+	thread_wakeup(ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
