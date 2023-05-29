f9f2b126378d248a30c39e971da34cd6d4feddf9
uujeen <rkddmlwls2@gmail.com>
2023-05-30 06:06:32 +0900

    :white_check_mark: Complete alarm-priority
    complete alarm-priority thread.c, .h file

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -147,4 +147,5 @@ void do_iret (struct intr_frame *tf);
 void thread_sleep(int64_t ticks);
 void thread_wakeup(int64_t ticks);
 bool list_ascending_func (const struct list_elem *a, const struct list_elem *b, void *aux);
+bool cmp_priority (const struct list_elem *a, const struct list_elem *b, void *aux);
 #endif /* threads/thread.h */
--- threads/thread.c
+++ threads/thread.c
@@ -208,7 +208,8 @@ thread_create (const char *name, int priority,
 
 	/* Add to run queue. */
 	thread_unblock (t);
-
+	def_thread *curr = thread_current();
+	thread_set_priority(curr->priority);
 	return tid;
 }
 
@@ -242,7 +243,7 @@ thread_unblock (struct thread *t) {
 
 	old_level = intr_disable ();
 	ASSERT (t->status == THREAD_BLOCKED);
-	list_push_back (&ready_list, &t->elem);
+	list_insert_ordered(&ready_list, &t->elem, cmp_priority, NULL);
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
 }
@@ -305,7 +306,7 @@ thread_yield (void) {
 
 	old_level = intr_disable ();
 	if (curr != idle_thread)
-		list_push_back (&ready_list, &curr->elem);
+		list_insert_ordered(&ready_list, &curr->elem, cmp_priority, NULL);
 	do_schedule (THREAD_READY);
 	intr_set_level (old_level);
 }
@@ -313,7 +314,18 @@ thread_yield (void) {
 /* Sets the current thread's priority to NEW_PRIORITY. */
 void
 thread_set_priority (int new_priority) {
-	thread_current ()->priority = new_priority;
+	//thread_current ()->priority = new_priority;
+	def_thread *curr = thread_current();
+	curr->priority = new_priority;
+	enum intr_level old_level;
+	old_level = intr_disable();
+
+	struct list_elem *ready_head = list_begin(&ready_list);
+	def_thread *cmp_t = list_entry(ready_head, def_thread, elem);
+	if (curr->priority < cmp_t->priority) {
+		thread_yield();
+	}
+	intr_set_level(old_level);
 }
 
 /* Returns the current thread's priority. */
@@ -598,6 +610,13 @@ bool list_ascending_func (const struct list_elem *a, const struct list_elem *b, 
 	def_thread *thread_b = list_entry(b, def_thread, elem);
 	
 	return thread_a->wakeup_tick < thread_b->wakeup_tick;
+}
+
+bool cmp_priority (const struct list_elem *a, const struct list_elem *b, void *aux) {
+	def_thread *thread_a = list_entry(a, def_thread, elem);
+	def_thread *thread_b = list_entry(b, def_thread, elem);
+	
+	return thread_a->priority > thread_b->priority;
 }
 
 /**
