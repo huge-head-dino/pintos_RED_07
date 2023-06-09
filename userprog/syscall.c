03ff811bae6bcd67604a3dea8617d6cc09236e4f
성환 <psung9510@gmail.com>
2023-06-10 02:48:52 +0900

    :memo: Update file related func

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -7,10 +7,28 @@
 #include "userprog/gdt.h"
 #include "threads/flags.h"
 #include "intrinsic.h"
+#include "filesys/filesys.h"
+#include "filesys/file.h"
+#include "threads/synch.h"
 
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
+void halt (void);
+void exit (int status);
+tid_t fork (const char *thread_name);
+int exec (const char *cmd_line);
+int wait (tid_t pid);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
 
+void check_address(void* addr);
 /* System call.
  *
  * Previously system call services was handled by the interrupt handler
@@ -35,12 +53,188 @@ syscall_init (void) {
 	 * mode stack. Therefore, we masked the FLAG_FL. */
 	write_msr(MSR_SYSCALL_MASK,
 			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+	
+	lock_init(&filesys_lock);
 }
 
 /* The main system call interface */
 void
 syscall_handler (struct intr_frame *f UNUSED) {
 	// TODO: Your implementation goes here.
+	switch(f->R.rax){
+		case SYS_HALT:
+			halt();
+		case SYS_EXIT:
+			exit(f->R.rdi);
+		case SYS_FORK:
+			fork(f->R.rdi);
+		case SYS_EXEC:
+			exec(f->R.rdi);
+		case SYS_WAIT:
+			wait(f->R.rdi);
+		case SYS_CREATE:
+			create(f->R.rdi, f->R.rsi);
+		case SYS_REMOVE:
+			remove(f->R.rdi);
+		case SYS_OPEN:
+			open(f->R.rdi);
+		case SYS_FILESIZE:
+			filesize(f->R.rdi);
+		case SYS_READ:
+			read(f->R.rdi, f->R.rsi, f->R.rdx);
+		case SYS_WRITE:
+			write(f->R.rdi, f->R.rsi, f->R.rdx);
+		case SYS_SEEK:
+			seek(f->R.rdi, f->R.rsi);
+		case SYS_TELL:
+			tell(f->R.rdi);
+		case SYS_CLOSE:
+			close(f->R.rdi);
+	}
+
 	printf ("system call!\n");
 	thread_exit ();
 }
+
+/*------------------------------------*/
+void halt (void){
+	power_off();
+}
+
+void exit (int status){
+	def_thread *curr = thread_current();
+	printf("%s: exit(%d) \n", curr->name, status);
+	thread_exit();
+}
+int exec (const char *cmd_line){}
+int wait (tid_t pid){}
+/*------------------------------------*/
+
+/*------------------------------------*/
+bool create (const char *file, unsigned initial_size){
+	check_address(file);
+	
+	if(filesys_create(file, initial_size)){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool remove (const char *file){
+	check_address(file);
+
+	if(filesys_remove(file)){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+int open (const char *file){
+	check_address(file);
+	struct file *f = filesys_open(file);
+	if(f==NULL){
+		return -1;
+	}
+
+	// 파일 객체에 대한 fd 생성.
+	int fd;
+	def_thread *curr = thread_current();
+	struct file **fdt = curr->fdt;
+
+	fd = curr->next_fd;
+	while(curr->fdt[fd]!=NULL){
+		fd++;
+	}
+
+	curr->next_fd = fd;
+	fdt[fd] = f;
+
+	if(fd == -1){
+		file_close(f);
+	}else{
+		return fd;
+	}
+}
+
+int filesize (int fd){
+	if(fd < 0){
+		return NULL;
+	}
+
+	// 프로세스의 fdt를 검색해서 파일 객체를 찾음.
+	def_thread *curr = thread_current();
+	struct file **fdt = curr->fdt;
+	struct file *file = fdt[fd];
+	
+	check_address(file);
+
+	if(file == NULL){
+		return -1;
+	}else{
+		file_length(file);
+	}
+}
+
+int read (int fd, void *buffer, unsigned size){}
+int write (int fd, const void *buffer, unsigned size){}
+
+// 파일을 특정위치로 이동시키는 함수.
+void seek (int fd, unsigned position){
+	if(fd < 2){
+		return;
+	}
+
+	// 프로세스의 fdt를 검색해서 파일 객체를 찾음.
+	def_thread *curr = thread_current();
+	struct file **fdt = curr->fdt;
+	struct file *file = fdt[fd];
+
+	check_address(file);
+
+	if(file == NULL){
+		return;
+	}else{
+		file_seek(file,position);
+	}
+}
+
+// 파일의 현재위치를 반환하는 함수.
+unsigned tell (int fd){
+	if(fd < 2){
+		return;
+	}
+
+	// 프로세스의 fdt를 검색해서 파일 객체를 찾음.
+	def_thread *curr = thread_current();
+	struct file **fdt = curr->fdt;
+	struct file *file = fdt[fd];
+
+	check_address(file);
+
+	if(file == NULL){
+		return;
+	}else{
+		return file_tell(fd);
+	}
+}
+
+void close (int fd){
+	def_thread *curr = thread_current();
+	struct file **fdt = curr->fdt;
+	struct file *file = fdt[fd];
+
+	file_close(file);
+}
+/*------------------------------------*/
+
+tid_t fork (const char *thread_name){}
+
+void check_address(void* addr){
+	if(!is_user_vaddr(addr) || addr ==NULL){
+		exit(-1);
+	}
+}
+
+
